e7f28c32afbd6c0a8e246efd49cc7513ea71bbe9
lilpolymath <ayofavour7@gmail.com>
2020-07-21 05:18:58 +0100

    feat: setup dark and light mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,7 @@ import { ReactComponent as Night } from '../assets/night.svg';
 
 import styles from './Navbar.module.css';
 
-// Honestly I don't know how Mobile first design works
-// so here is my lazy attempt at making that.
-
-// Omo!! all the best let's see how this turns out.
-
 const Navbar = () => {
-  // Control the fill opacity of the svg with hooks.
-  // Might need to move this to the index to have a base level control
-  // SMH!! I used this method in my last project ooo
-
   const [day, setDay] = useState(true);
   const [openMenu, setOpenMenu] = useState(false);
 
@@ -22,16 +13,34 @@ const Navbar = () => {
     setOpenMenu(!openMenu);
   };
 
+  const switchBG = () => {
+    setDay(!day);
+    document.documentElement.style.setProperty(
+      '--first',
+      day ? '#f2994a' : '#bdbdbd'
+    );
+    document.documentElement.style.setProperty(
+      '--second',
+      day ? '#bdbdbd' : '#f2994a'
+    );
+    document.documentElement.style.setProperty(
+      '--main',
+      day ? '#ffffff' : '#1a181b'
+    );
+    document.documentElement.style.setProperty(
+      '--text',
+      day ? '#1a181b' : '#ffffff'
+    );
+  };
+
   return (
     <nav className={styles.nav}>
       <div className={styles.logo}>
-        <Day style={{ fill: 'red' }} />
+        <Day onClick={switchBG} className={styles.first} />
         <h1 className={styles.logo_text}>Ayobami Adedapo</h1>
-        <Night />
+        <Night onClick={switchBG} className={styles.second} />
       </div>
-      <button onClick={toggleMenubar} className={styles.menu}>
-        Menu
-      </button>
+      <button className={styles.menu_button}>Menu</button>
       <div
         style={{ display: openMenu ? 'block' : 'none' }}
         className={styles.menubar}
