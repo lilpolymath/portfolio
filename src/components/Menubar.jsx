66099abc8634217c4d4ea38deff00170e46b0cc4
lilpolymath <ayofavour7@gmail.com>
2020-08-21 17:49:59 +0100

    feat: menubar link mouse hover effect

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Navbar.module.css';
 import { useSpring, animated, config } from 'react-spring';
 
-const Menubar = ({ open, toggleMenubar }) => {
+const Menubar = ({ open, toggleMenubar, mouseEnter, mouseLeave }) => {
   const sidebar = useSpring({
     transform: open ? `translateX(0)` : `translateX(120%)`,
     maxWidth: open ? '500px' : '0px',
@@ -12,27 +12,52 @@ const Menubar = ({ open, toggleMenubar }) => {
 
   return (
     <animated.div style={sidebar} className={styles.menubar}>
-      <button onClick={toggleMenubar} className={styles.menubar_close}>
+      <button
+        onClick={toggleMenubar}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+        className={styles.menubar_close}
+      >
         Close
       </button>
       <ul>
         <li>
-          <a className={styles.menubar_link} href='/home'>
+          <a
+            className={styles.menubar_link}
+            href='/home'
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}
+          >
             Home
           </a>
         </li>
         <li>
-          <a className={styles.menubar_link} href='/projects'>
+          <a
+            className={styles.menubar_link}
+            href='/projects'
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}
+          >
             Projects
           </a>
         </li>
         <li>
-          <a className={styles.menubar_link} href='/blog'>
+          <a
+            className={styles.menubar_link}
+            href='/blog'
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}
+          >
             Blog
           </a>
         </li>
         <li>
-          <a className={styles.menubar_link} href='/contact'>
+          <a
+            className={styles.menubar_link}
+            href='/contact'
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}
+          >
             Contact
           </a>
         </li>
