249d4ed4f5db0db107d76ec88b9074bb6a43a7f6
lilpolymath <ayofavour7@gmail.com>
2020-08-21 14:43:06 +0100

    feat: breadcrumbs and spacing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,41 +2,68 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { animated } from 'react-spring/renderprops';
 
+// - TODO
+// - Add social icons : Twitter, GitHub, LinkedIn, Dribbble, Codepen
+
 import styles from './Contact.module.css';
 
 const Intro = ({ style, mouseEnter, mouseLeave }) => {
   return (
     <animated.section style={style} className={styles.contact}>
-      <div className={styles.image}>
-        <figure>
-          <img
-            src='https://res.cloudinary.com/favourcodes/image/upload/v1597784911/profile.png'
-            alt='ayobami favour adedapo'
-          />
-        </figure>
+      <div
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+        className={styles.breadcrumb}
+      >
+        <Link to='/home'>
+          <span className={styles.inactive}>Home</span>
+        </Link>
+        <span className={styles.active}>/Contact</span>
       </div>
-      <div className={styles.contact_content}>
-        <header>
-          <h3 className={styles.heading}>Contact Information</h3>
-        </header>
-        <p className={styles.contact_bait}>
-          Socrates set the agenda for the tradition of critical thinking,
-          namely, to reflectively question common beliefs and explanations,
-          carefully distinguishing those beliefs that are reasonable and logical
-          from those which — however appealing they may be to our native
-          egocentrism.
-        </p>
-        <div className={styles.contact_options}>
-          <div className={styles.contact_option}>
-            <p>Email</p>
-            <a href='mailto:ayofavour7@gmail.com'>ayofavour7@gmail.com</a>
-          </div>
-          <div className={styles.contact_option}>
-            <p>Phone Number</p>
-            <a href='tel:+2348143567540'>+234 814 356 7540</a>
+      <section className={styles.main}>
+        <div className={styles.image}>
+          <figure>
+            <img
+              src='https://res.cloudinary.com/favourcodes/image/upload/v1597784911/profile.png'
+              alt='ayobami favour adedapo'
+            />
+          </figure>
+        </div>
+        <div className={styles.contact_content}>
+          <header>
+            <h3 className={styles.heading}>Contact Information</h3>
+          </header>
+          <p className={styles.contact_bait}>
+            Socrates set the agenda for the tradition of critical thinking,
+            namely, to reflectively question common beliefs and explanations,
+            carefully distinguishing those beliefs that are reasonable and
+            logical from those which — however appealing they may be to our
+            native egocentrism.
+          </p>
+          <div className={styles.contact_options}>
+            <div className={styles.contact_option}>
+              <p>Email</p>
+              <a
+                onMouseEnter={mouseEnter}
+                onMouseLeave={mouseLeave}
+                href='mailto:ayofavour7@gmail.com'
+              >
+                ayofavour7@gmail.com
+              </a>
+            </div>
+            <div className={styles.contact_option}>
+              <p>Phone Number</p>
+              <a
+                onMouseEnter={mouseEnter}
+                onMouseLeave={mouseLeave}
+                href='tel:+2348143567540'
+              >
+                +234 814 356 7540
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </animated.section>
   );
 };
